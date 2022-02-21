6ebeeff70e2d3b6e359e7555e80539e7a131248e
Ji Yeong Seo <tj5134@naver.com>
2022-02-21 21:01:06 +0900

    템플릿 내 조건문

--- vue/vue-3-to-learn-in-one-book/hello-world-vite2/src/App.vue
+++ vue/vue-3-to-learn-in-one-book/hello-world-vite2/src/App.vue
@@ -2,11 +2,13 @@
 import DeclearativeRendering from './components/DeclearativeRendering.vue';
 import CombineData from './components/CombineData.vue';
 import EventListener from './components/EventListener.vue';
+import TemplateConditional from './components/TemplateConditional.vue';
 </script>
 
 <template>
   <DeclearativeRendering />
   <CombineData />
   <EventListener />
+  <TemplateConditional />
 </template>
 
