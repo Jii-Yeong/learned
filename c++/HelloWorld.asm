397c8f64f7a5e0357803bf3cce1a308af9dde9af
Jii-Yeong <tj5134@naver.com>
2022-04-17 12:23:41 +0900

    [c++] 데이터 기초

--- c++/HelloWorld.asm
+++ c++/HelloWorld.asm
@@ -4,6 +4,19 @@ section .text
 global CMAIN
 CMAIN:
     ;write your code here
+    
+    ; 10진수(0 1 2 3 4 5 6 7 8 9)
+    ; 10 11 12 13 ... 19 20
+    
+    ; 2진수(0 1)
+    ; 01 10
+    ; 0b0 0b1 0b10 0b11 0b100
+     
+    ; 16진수(0 1 2 3 4 5 6 7 8 9 A B C D E F)
+    ; 0 1 2 3 4 5 6 7 8 9 A B C D F 10
+    ; 0x00
+    
+    ; 0b10010101 = 0x95
     PRINT_STRING msg
     xor rax, rax
     ret
